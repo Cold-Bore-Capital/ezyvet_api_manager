485fed462686e4a3bfa45cc31e7e031a4cc732a1
Craig Lathrop <clathrop@coldborecapital.com>
2021-09-12 15:39:45 -0700

    Fixed up migrated unit tests.

---
+++ tests/models/test_appointments.py
@@ -0,0 +1,38 @@
+from unittest import TestCase
+
+import pandas as pd
+
+from ezyvetapi.models.appointments import Appointments
+
+
+class TestAppointments(TestCase):
+
+    def test__truncate_description_col(self):
+        pass
+
+
+    def test__remove_columns(self):
+        pass
+
+    def test__set_data_types(self):
+        pass
+
+    def test__remove_block_out_bookings(self):
+        pass
+
+    def test__add_resource_data(self):
+        pass
+
+    def test__set_primary_resource_id(self):
+        pass
+
+    def test__translate_id_fields(self):
+        pass
+
+
+
+
+class MockDBManager:
+
+    def __init__(self, **kwargs):
+        pass
