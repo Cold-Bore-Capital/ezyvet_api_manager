201e9e2afb594148e3ee3681491960b32b6f9bbc
Craig Lathrop <clathrop@coldborecapital.com>
2021-12-30 19:52:31 -0700

    changing location of the models.

--- ezyvetapi/__init__.py
+++ ezyvetapi/__init__.py
@@ -1,6 +1,1 @@
 from ezyvetapi.main import EzyVetApi
-from ezyvetapi.models.appointments import Appointments as appointments
-from ezyvetapi.models.animals import Animals as animals
-from ezyvetapi.models.contacts import Contacts as contacts
-from ezyvetapi.models.resources import Resources as resources
-from ezyvetapi.models.shelter_animal_bookings import ShelterAnimalBooking as shelter_animal_bookings
